afc06a665809aeb2e51dc34b7c8c417859219b21
Rafaela Mendes <81865736+Rafa-MMf@users.noreply.github.com>
2021-08-19 22:41:14 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,327 @@
+var cartaProfessor = {
+    nome: "Professor X",
+    imagem: "https://segredosdomundo.r7.com/wp-content/uploads/2020/09/professor-xavier-origem-historia-e-poderes-do-criador-dos-x-men.jpg",
+    atributos: {
+        ataque: 10,
+        defesa: 70,
+        magia: 100
+    }
+}
+
+var cartaCiclope = {
+    nome: "Ciclope",
+    imagem: "https://i.pinimg.com/originals/f5/f7/56/f5f7561d77cf7452b402a4087a7a7a42.jpg",
+    atributos: {
+        ataque: 80,
+        defesa: 70,
+        magia: 30
+    }
+}
+
+var cartaFenix = {
+    nome: "Fenix",
+    imagem: "https://i.pinimg.com/originals/2c/98/9a/2c989a7c22a55450d811b99bd2d0f18c.jpg",
+    atributos: {
+        ataque: 100,
+        defesa: 100,
+        magia: 100
+    }
+}
+
+var cartaTempestade = {
+    nome: "Tempestade",
+    imagem: "https://pm1.narvii.com/6496/c9b2600ef9c11b08389ac27c4300be3c9d223c61_hq.jpg",
+    atributos: {
+        ataque: 80,
+        defesa: 40,
+        magia: 70
+    }
+}
+
+var cartaNoturno = {
+    nome: "Noturno",
+    imagem: "http://3.bp.blogspot.com/-ZuL_6rQUktM/VT5av08YOpI/AAAAAAAAKg0/s7mDPQhswOI/s1600/03.jpg",
+    atributos: {
+        ataque: 40,
+        defesa: 70,
+        magia: 50
+    }
+}
+
+var cartaLince = {
+    nome: "Lince Negra",
+    imagem: "https://i.pinimg.com/originals/9b/91/db/9b91dbc85db8913f9d55bc8ea5be26c6.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 90,
+        magia: 80
+    }
+}
+
+var cartaVampira = {
+    nome: "Vampira",
+    imagem: "https://i.ytimg.com/vi/9Jcwk3Idb34/maxresdefault.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 60,
+        magia:90
+    }
+}
+
+var cartaFera = {
+    nome: "Fera",
+    imagem: "https://i.pinimg.com/600x315/3a/24/24/3a24242b82d9a41ce75a5799af792607.jpg",
+    atributos: {
+        ataque: 80,
+        defesa: 70,
+        magia: 0
+    }
+}
+
+var cartaWolverine = {
+    nome: "Wolverine",
+    imagem: "https://assets.puzzlefactory.pl/puzzle/300/044/original.jpg",
+    atributos: {
+        ataque: 90,
+        defesa: 80,
+        magia: 0
+    }
+}
+
+var cartaGelo = {
+    nome: "Homem de Gelo",
+    imagem: "https://static.wikia.nocookie.net/xmen-comics/images/8/86/Astonishing_X-Men_Annual_Vol_2_1_Hyung_Variant_Textless.jpg/revision/latest?cb=20200425153756&path-prefix=pt-br",
+    atributos: {
+        ataque: 100,
+        defesa: 30,
+        magia: 70
+    }
+}
+
+var cartaDinamite = {
+    nome: "Dinamite",
+    imagem: "https://lh3.googleusercontent.com/proxy/dy89flKduaJNlBBD0gRNRYs_qXc8lY5gPhXnUeNqiVER7AVhgwEs5Xz7r2-OPOHKPDrhGzoVFWtOuVTEW5_MI6J8eLh6rS9sgbcel1m5HoyVA4O7IHO9mUzX10fh3J0ESUGLiQ1B-Q",
+    atributos: {
+        ataque: 70,
+        defesa: 60,
+        magia: 70
+    }
+}
+
+var cartaMancha = {
+    nome: "Mancha Solar",
+    imagem: "https://lh3.googleusercontent.com/proxy/8Q_Xs57_bDztL8HYPPfohTHMl_JJeFTp87_6LRl9S8ThS6jELaGd3FhARuyaKCj5yf9p1gHBRbp1gd92A5pdA4mG_sbRvOYq0wMtm9k0G3OBPurXDyqog9t6wYv-JNv7U5UClE4BsQ",
+    atributos: {
+        ataque: 80,
+        defesa: 60,
+        magia: 80
+    }
+}
+
+var cartaMagneto = {
+    nome: "Magneto",
+    imagem: "https://rollingstone.uol.com.br/media/_versions/magneto-thumb-pequena-reprod-marvel-comics_widelg.jpg",
+    atributos: {
+        ataque: 100,
+        defesa: 90,
+        magia: 80
+    }
+}
+
+var cartaDentes = {
+    nome: "Dentes de Sabre",
+    imagem: "https://lh3.googleusercontent.com/proxy/L9llUx4OYAWCGJ7yMS-0N5IRfDrR64x_2N50tdXBx-9W6Al-q-THB4qT3Gmhf4x2YCNoecFny8_m0pOyXr70xmuPw5qLs-gHrQFuvHjZtoZschsX8zTwL5NfHeFtPjJU2J3jA3jXkQ",
+    atributos: {
+        ataque: 80,
+        defesa: 80,
+        magia: 0
+    }
+}
+
+var cartaColossus  = {
+    nome: "Colossus",
+    imagem: "https://mateusstein.files.wordpress.com/2008/03/colossus_by_ryankinnaird.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 100,
+        magia: 10
+    }
+}
+
+var cartaMistica = {
+    nome: "Mística",
+    imagem: "https://static.wikia.nocookie.net/xmen-comics/images/9/9d/X-Men_Vol_5_4_Maer_Virgin_Variant.jpg/revision/latest?cb=20200607142914&path-prefix=pt-br",
+    atributos: {
+        ataque: 50,
+        defesa: 85,
+        magia: 80
+    }
+}
+
+var cartaMercurio = {
+    nome: "Mercúrio",
+    imagem: "http://pm1.narvii.com/6710/c137550f28b5a4ed51db507ae134fe517c46ba2a_00.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 40,
+        magia: 0
+    }
+}
+
+var cartaFeiticeira = {
+    nome: "Feiticeira Escarlate",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/9/91/Feiticeira_Escarlate.jpg",
+    atributos: {
+        ataque: 100,
+        defesa: 70,
+        magia: 100
+    }
+}
+
+var cartaDestrutor = {
+    nome: "Destrutor",
+    imagem: "https://s.aficionados.com.br/imagens/destrutor_f.jpg",
+    atributos: {
+        ataque: 83,
+        defesa: 80,
+        magia: 85
+    }
+}
+
+var cartaGambit = {
+    nome: "Gambit",
+    imagem: "https://static.wikia.nocookie.net/xmen-comics/images/0/0c/Excalibur_Vol_4_2_Parrillo_Virgin_Variant.jpg/revision/latest?cb=20200425151250&path-prefix=pt-br",
+    atributos: {
+        ataque: 70,
+        defesa: 60,
+        magia: 80
+    }
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaProfessor, cartaCiclope, cartaFenix, cartaTempestade, cartaNoturno, cartaLince, cartaVampira, cartaFera, cartaWolverine, cartaGelo, cartaDinamite, cartaMancha, cartaMagneto, cartaDentes, cartaColossus, cartaMistica, cartaMercurio, cartaFeiticeira, cartaDestrutor, cartaGambit]     
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas() {
+  var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+  var html = "Quantidade de cartas no jogo: " + cartas.length
+  
+  divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar() {
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + " / " + pontosMaquina + " Máquina"
+  
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+    exibeCartaJogador()
+}
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>'
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>'
+        pontosMaquina++
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+  
+  if (cartas.length == 0) {
+    alert("Fim de jogo")
+    if (pontosJogador > pontosMaquina){
+      htmlResultado = '<p class="resultado-final">Venceu</p>'
+    } else if (pontosMaquina > pontosJogador) {
+      htmlResultado = '<p class="resultado-final">Perdeu</p>'
+    } else {
+      htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+  } else {
+    document.getElementById('btnProximaRodada').disabled = false
+  }
+
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true
+    
+    atualizaPlacar()
+    exibeCartaMaquina()
+    atualizaQuantidadeDeCartas()
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        console.log(atributo)
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function proximaRodada() {
+  var divCartas = document.getElementById('cartas')
+  
+  divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+  document.getElementById('btnSortear').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  document.getElementById('btnProximaRodada').disabled = true
+  
+  var divResultado = document.getElementById('resultado')
+  divResultado.innerHTML = ""
+}
